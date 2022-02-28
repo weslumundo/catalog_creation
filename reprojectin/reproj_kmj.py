ecb9402194d92d4f442fc4df75e3d95664913309
weslumundo <64718224+weslumundo@users.noreply.github.com>
2022-02-27 20:48:10 -0600

    Removed unessesary \n s

--- reprojectin/reproj_kmj.py
+++ reprojectin/reproj_kmj.py
@@ -10,15 +10,15 @@ import sys
 fileblue=sys.argv[1]
 filered=sys.argv[2]
 fileout=sys.argv[3]
-print("Opening a file\n")
+print("Opening a file")
 print(filered)
 hrp=fits.open(filered)
-print("Opening a file\n")
+print("Opening a file")
 print(fileblue)
 hop=fits.open(fileblue)
-print("Starting reproject_exact\n")
+print("Starting reproject_exact")
 array2,footprint1= reproject_exact(hop,hrp[0].header)
-print("Finished reproject_exact\n")
+print("Finished reproject_exact")
 
 fits.writeto(fileout, array2, hrp[0].header, overwrite=True)
 print('done reprojectin sci')
@@ -40,10 +40,10 @@ tmpolist=tmpo.split('/')
 tmponame=tmpolist[len(tmpolist)-1]
 outwht=tmponame.replace('.fits','_wht.fits')
 
-print("Opening a file\n")
+print("Opening a file")
 print(whtfileblue)
 wb=fits.open(whtfileblue)
-print("Opening a file\n")
+print("Opening a file")
 print(whtfilered)
 wr=fits.open(whtfilered)
 whtarray2,footprintwht=reproject_exact(wb,wr[0].header)
