0f50a95c9a8c2b08a5605ff06a770c5a371d2461
weslumundo <64718224+weslumundo@users.noreply.github.com>
2022-02-27 20:25:27 -0600

    Added debug prints

--- reprojectin/reproj_kmj.py
+++ reprojectin/reproj_kmj.py
@@ -10,9 +10,15 @@ import sys
 fileblue=sys.argv[1]
 filered=sys.argv[2]
 fileout=sys.argv[3]
+print("Opening a file\n")
+print(filered)
 hrp=fits.open(filered)
+print("Opening a file\n")
+print(fileblue)
 hop=fits.open(fileblue)
+print("Starting reproject_exact\n")
 array2,footprint1= reproject_exact(hop,hrp[0].header)
+print("Finished reproject_exact\n")
 
 fits.writeto(fileout, array2, hrp[0].header, overwrite=True)
 print('done reprojectin sci')
@@ -34,8 +40,11 @@ tmpolist=tmpo.split('/')
 tmponame=tmpolist[len(tmpolist)-1]
 outwht=tmponame.replace('.fits','_wht.fits')
 
-
+print("Opening a file\n")
+print(whtfileblue)
 wb=fits.open(whtfileblue)
+print("Opening a file\n")
+print(whtfilered)
 wr=fits.open(whtfilered)
 whtarray2,footprintwht=reproject_exact(wb,wr[0].header)
 fits.writeto(outwht,whtarray2,wr[0].header,overwrite=True)
