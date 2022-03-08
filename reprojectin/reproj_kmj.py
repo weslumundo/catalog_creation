11e0d767038a74871697aad68a531f211f136f8e
Gregory Rudnick <grudnick@ku.edu>
2022-03-08 11:38:43 -0600

    added comments on how to replace header keywords

--- reprojectin/reproj_kmj.py
+++ reprojectin/reproj_kmj.py
@@ -7,19 +7,34 @@ from astropy.convolution import Gaussian2DKernel
 from astropy.convolution import convolve
 import sys
 
-fileblue=sys.argv[1]
-filered=sys.argv[2]
-fileout=sys.argv[3]
+#reprojects a short wavelength ("blue") image onto a longer wavelength ("red") image
+#reproject uses the image header to determine the reprojection, and so relies on
+#an accurate WCS in both images
+
+#takes three inputs at the command line
+fileblue=sys.argv[1]   #short wavelength image
+filered=sys.argv[2]   #long wavelenght image
+fileout=sys.argv[3]    #output image
 print("Opening a file")
 print(filered)
 hrp=fits.open(filered)
 print("Opening a file")
 print(fileblue)
 hop=fits.open(fileblue)
+
+#do the reprojecting
 print("Starting reproject_exact")
 array2,footprint1= reproject_exact(hop,hrp[0].header)
 print("Finished reproject_exact")
 
+#this outputs the reprojected image .  It currently writes the red header to the reprojected file
+#we need to:
+#1. copy the blue header to a new structure
+#2. replace the WCS keywords with those from the red header
+#3. use this new header as the one we write to the file.  This preserves all of the filter-specific
+#information but includes the right header
+#We want to replace the following header keywords: WCSAXES, CRPIX1, CRPIX2, CRVAL1, CRVAL2, CTYPE1, CTYPE2, ORIENTAT, VAFACTOR, CD1_1, CD1_2, CD2_1, CD2_2
+#for example, hrp[0].header['CD1_1'] contains the value for that header item 
 fits.writeto(fileout, array2, hrp[0].header, overwrite=True)
 print('done reprojectin sci')
 
