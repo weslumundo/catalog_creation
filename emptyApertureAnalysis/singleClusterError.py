2c4ce1d20e54a353f7ff8ee66e30c1832ddeed9d
weslumundo <sportsmanwesley@gmail.com>
2022-12-04 20:56:48 -0600

    c values output as a txt in output folder

--- emptyApertureAnalysis/singleClusterError.py
+++ emptyApertureAnalysis/singleClusterError.py
@@ -20,7 +20,8 @@
 #Weight Files should end in '_wht.fits'
 #The default_objsub_norm.sex is in the same folder as this file
 #The exported version of maskitextra.py is in the same folder as this file
-#Assumes the user wants the txt file in the directory that outputBase points toward(sci file root), not the directory in the txtLocationVariable [THIS NEEDS CHANGED]
+#Assumes the user wants the txt file in the directory that outputBase points toward(sci file root), not the directory in the txtLocationVariable [THIS SHOULD BE CHANGED]
+
 
 import sys
 from astropy.io import fits
@@ -49,6 +50,8 @@ class singleClustorErr:
 	objsubSex = ""
 	pixScale = 0.1
 	cvalues = 0
+	baseName=""
+	cvaluesLocation = ""
 	
 	#the self is some python witchcraft, just pretend it isnt there
 	def __init__(self, myScienceFileLocation):
@@ -74,12 +77,20 @@ class singleClustorErr:
 		#stole this idea from KMJ
 		self.apprFluxName = nameLocationList[len(nameLocationList)-1].replace('_sci.fits','_apprFlux')
 		self.outputTxtName = nameLocationList[len(nameLocationList)-1].replace('_sci.fits','_apprFlux.txt')
+		
+		self.baseName = nameLocationList[len(nameLocationList)-1].replace('_sci.fits','')
+		cName = nameLocationList[len(nameLocationList)-1].replace('_sci.fits','_cvalues.txt')
+		 
 		#quickly find the 'root' folder
 		myRoot = ""
 		for i in range(0,len(nameLocationList)-1):
 			myRoot += nameLocationList[i] + "/"
 		self.outputBase = myRoot
 		#self.outputLocation = self.outputBase+self.apprFluxName+"/"
+		
+		#attach cvaluelocation to output folder
+		self.cvaluesLocation = self.outputBase + cName
+		
 
 		
 	#creates the Normalized and Masked files, if passed true it will overwrite at these locations	
@@ -118,17 +129,39 @@ class singleClustorErr:
 
 	def begin(self):
 		#This function autoruns the singleClusterErr process
-		myError.generateNormal()		
-		myError.generateObjSub()
-		myError.generateObjSubMask()
-		myError.startEAA()
-		myError.generateTxt()
-		myError.generateC()
+		self.preEAA()
+		self.startEAA()
+		self.postEAA()
+		
+	def preEAA(self):
+		self.generateNormal()		
+		self.generateObjSub()
+		self.generateObjSubMask()
+	
+	def postEAA(self):
+		self.generateTxt()
+		self.generateC() 
+		self.outputC()
+		
+	def genText(self):
+		#creates the contents of the cvalue output file
+		myOut  = ""
+		myOut += "Name\t"+self.baseName+'\n'
+		myOut += "c0\t"+str(self.cvalues.c0.value)+'\n'
+		myOut += "c1\t"+str(self.cvalues.c1.value)+'\n'
+		myOut += "c2\t"+str(self.cvalues.c2.value)+'\n'
+		return myOut
 
+	def outputC(self):
+		myFile = open(self.cvaluesLocation,"w")
+		myFile.write(self.genText())
+		myFile.close()
 
 myError = singleClustorErr(sys.argv[1])
 #this is where the user has the chance to manually change any of the defaults defined in init 
 myError.setPixScale(0.1)
-myError.begin()	
+myError.begin()
+
+	
 		
 		
