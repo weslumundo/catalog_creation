583a97b53cc349cebbc3f5246c72db6a976d9134
Gregory Rudnick <grudnick@ku.edu>
2023-02-09 15:33:35 -0600

    added comments and header README

--- emptyApertureAnalysis/maskitextrav2.py
+++ emptyApertureAnalysis/maskitextrav2.py
@@ -6,6 +6,21 @@ import astropy
 from astropy.io import fits
 import matplotlib.pyplot as plt
 
+'''
+
+take an input image and inverse variance weight map, create a
+noise-normalized image, and output it.
+
+The output image will not have usable fluxes for the data but the
+noise properties are homogenized.  This makes it suitable for an empty
+aperture simulation to measure the depth across the image.
+
+INPUT REQUIREMENTS: 
+
+The WHT file must be an IVAR map.
+
+'''
+
 def run(sciFileLoc,whtFileLoc,normFileLoc,maskFileLoc,ow=True):
 	#print("Succ")
 
@@ -20,12 +35,13 @@ def run(sciFileLoc,whtFileLoc,normFileLoc,maskFileLoc,ow=True):
 
 
 	#########construct the normalization factor fn
-	#first, select only the parts of the weight image where data exist
+	#first, select only the parts of the weight image where data exist.  This will break if an RMS map is inserted instead.
 	dat=data2[np.where(data2 >=2)]
 	#next, flatten that to an array of all values in the data, and sort it
 	dat2=dat.flatten()
 	sortwht=np.sort(dat2)
 	#find the position of the 1% highest value within the sorted array; it should be at the 1% position in the array
+    #this is used to normalize the IVAR map by its (near) maximum value.
 	pt9perct=round(0.01*len(sortwht))
 	pt1perct=len(sortwht)-pt9perct
 	print('length and 1percent point',len(sortwht),pt1perct)
@@ -33,11 +49,13 @@ def run(sciFileLoc,whtFileLoc,normFileLoc,maskFileLoc,ow=True):
 	val1perct=sortwht[int(pt1perct)]
 	#compare that value (val1perct) with the value if I'd rounded up or down instead--this proves the values are 	virtually identical, so it doesn't matter
 	print('1percent points?',val1perct,sortwht[int(pt1perct)-1])
-	#so then the normalization factor fn is:
+
+    #the normalization factor fn normalizes the noise across the image.
 	fn=np.sqrt(data2)/np.sqrt(val1perct)
 	
 	
-	#######perform the normalization and output the data
+    #normalize the image to have the same noise by multipling the data by normalization map "fn"
+    #######perform the normalization and output the data
 	outdata=data1*fn
 	hdout=fits.PrimaryHDU(outdata,hdu1[0].header)
 	#WES: We Generate image names in init so this kind of code is no longer nessessary
@@ -53,7 +71,7 @@ def run(sciFileLoc,whtFileLoc,normFileLoc,maskFileLoc,ow=True):
 	#####below here, create a mask to use for removing non-zero values outside the data image created by convolution in 	sextractor
 	data3=hdu2[0].data
 		
-	i1=np.where(data3>2)
+	i1=np.where(data3>=2)
 	i0=np.where(data3<2)
 	
 	data3[i1]=1
--- emptyApertureAnalysis/quickmaskv2.py
+++ emptyApertureAnalysis/quickmaskv2.py
@@ -3,6 +3,33 @@ import os
 import sys
 import astropy
 from astropy.io import fits
+
+'''
+
+PURPOSE
+
+Create an image that is multipled by a mask whose zero values indicate
+no data.  In practice, a generic routine that multiplies two FITS images
+
+INPUT
+
+
+Takes the object subtracted image produced by SExtractor and
+multiplies it by the mask created by maskitextrav2.py.  The mask tells
+where this is and is not data.
+
+The input is not specific to a mask and image.  The variables in this
+code just call them as such.
+
+OUTPUT 
+
+The output image is set to zero (using the no-data mask values)
+wherever there is no data.
+
+
+'''
+
+
 def run(objsubFileLoc,maskFileLoc,objsubMaskFileLoc,ow = True):
 	objsubim=objsubFileLoc
 	maskim=maskFileLoc
