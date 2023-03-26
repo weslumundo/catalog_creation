c9693c561ebda2c4f2e5102cc8e0ff3b008d22c8
d3lwyrn <103296194+d3lwyrn@users.noreply.github.com>
2023-03-26 14:37:17 -0500

    Update reproject_main.py

--- reprojectin/reproject_main.py
+++ reprojectin/reproject_main.py
@@ -8,13 +8,35 @@ file_out = sys.argv[3]
 
 #reproject with the sci files using Reproject class
 r_sci = Reproject(file_ref, file_targ, file_out)
-r_sci.reprojector()
+r_sci.file_check()
+#r_sci.reprojector()
+print("SCI files reprojected.")
 
 #locate the wht files by changing the string Python looks for
 wht_file_ref = file_ref.replace('_sci.fits','_wht.fits')
 wht_file_targ = file_targ.replace('_sci.fits','_wht.fits')
 wht_file_out = file_out.replace('_sci.fits','_wht.fits')
 
-#reproject with the wht files using Reproject class
-r_wht = Reproject(wht_file_ref, wht_file_targ, wht_file_out)
-r_wht.reprojector()
+
+VALID = 0
+while VALID == 0:
+    user_response = input("Reproject associated WHT files? Y/N : ")
+    user_response = user_response.upper()
+    
+    if user_response == "Y":
+    
+        #reproject with the wht files using Reproject class
+        r_wht = Reproject(wht_file_ref, wht_file_targ, wht_file_out)
+        r_wht.file_check()
+        #r_wht.reprojector()
+        print("WHT files reprojected.")
+        VALID = 1
+        
+    elif user_response == "N":
+        VALID = 1
+    
+    else:
+        print("Invalid entry, please enter Y or N.")
+
+print("Exit")
+    
